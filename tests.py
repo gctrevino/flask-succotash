8c3bcd61297a616175bca6e954b465977bbfc590
Gilberto Treviño <TrevinoGilberto@JohnDeere.com>
2021-03-05 06:35:07 -0600

    Improved verbosity of test run

--- tests.py
+++ tests.py
@@ -10,25 +10,25 @@ class MyTestCase(unittest.TestCase):
     def test_sort_numbers(self):
         json_send = json.loads('{"data": "1,4,3,1,5,7,9,0,56,10"}')
         json_expected = json.loads('{"data": "0,1,1,3,4,5,7,9,10,56"}')
-        response = requests.put('http://127.0.0.1:5000/', json=json_send)
+        response = requests.put('http://0.0.0.0:5000/', json=json_send)
         json_response = json.loads(response.text)
-        print(f'{json_expected} == {json_response} ?')
+        print(f'\nExpected : {json_expected}\nActual   : {json_response}')
         self.assertEqual(json_expected, json_response)
 
     def test_sort_alpha(self):
         json_send = json.loads('{"data": "1,4,3,1,5,799,9,0,56,10"}')
         json_expected = json.loads('{"data": "0,1,1,3,4,5,9,10,56,799"}')
-        response = requests.put('http://127.0.0.1:5000/', json=json_send)
+        response = requests.put('http://0.0.0.0:5000/', json=json_send)
         json_response = json.loads(response.text)
-        print(f'{json_expected} == {json_response} ?')
+        print(f'\nExpected : {json_expected}\nActual   : {json_response}')
         self.assertEqual(json_expected, json_response)
 
     def test_sort_get(self):
         json_send = json.loads('{"data": "1,4,3,1,5,799,9,0,56,10"}')
         json_expected = json.loads('{"error": "Please use PUT or POST for your requests..."}')
-        response = requests.get('http://127.0.0.1:5000/', json=json_send)
+        response = requests.get('http://0.0.0.0:5000/', json=json_send)
         json_response = json.loads(response.text)
-        print(f'{json_expected} == {json_response} ?')
+        print(f'\nExpected : {json_expected}\nActual   : {json_response}')
         self.assertEqual(json_expected, json_response)
 
 
